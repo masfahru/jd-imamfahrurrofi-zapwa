5ce53fb47f67dffe719a68a6f762881a779b513c
masfahru <imamfahrurrofi@gmail.com>
2025-08-29 13:29:15 +0700

    feat: update database connection to use pg Pool and add dotenv example

--- server/src/core/db/drizzle.ts
+++ server/src/core/db/drizzle.ts
@@ -1,6 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import * as schema from "./schema";
 
-const client = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(client, { schema });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+export const db = drizzle(pool, { schema });
